eac07ce2440487aa189f7c9353e42ea3a416265d
sudharsan <sssudharsan@gmail.com>
2018-12-30 00:20:57 +0800

    changed files

--- config-server/src/main/java/com/sudharsan/ConfigServerApplication.java
+++ config-server/src/main/java/com/sudharsan/ConfigServerApplication.java
@@ -2,9 +2,11 @@ package com.sudharsan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
