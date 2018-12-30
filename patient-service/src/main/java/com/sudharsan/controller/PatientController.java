498a52f91c630f7007514adbf42301b647365722
sudharsan <sssudharsan@gmail.com>
2018-12-30 17:41:53 +0800

    file changed

--- patient-service/src/main/java/com/sudharsan/controller/PatientController.java
+++ patient-service/src/main/java/com/sudharsan/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.sudharsan.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,7 +17,8 @@ public class PatientController {
 	@Autowired
 	PatientService patientService;
 	
-	@PostMapping(path="/addPatient")
+	@PostMapping(path="/addPatient",consumes=MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<?> addPatient(@RequestBody Patient patient){
 		patient = patientService.addPatient(patient);
 		return new ResponseEntity<>(String.format("Patient Created Successfully"),HttpStatus.CREATED);
--- patient-service/src/main/java/com/sudharsan/serviceImpl/PatientProxy.java
+++ patient-service/src/main/java/com/sudharsan/serviceImpl/PatientProxy.java
@@ -1,6 +1,8 @@
 package com.sudharsan.serviceImpl;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +13,8 @@ public class PatientProxy {
 	public static final String URL="http://localhost:8080/doctor/getDoctor/";
 	
 	public static String getDoctorDetails(int id) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
 		String docInfo = URL+id;
 		return rt.getForObject(docInfo, String.class);
 	}
