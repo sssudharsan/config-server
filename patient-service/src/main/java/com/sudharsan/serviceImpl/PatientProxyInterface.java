78bf05b16bd62d8ac40bec50fb5d0566ff355f51
sudharsan <sssudharsan@gmail.com>
2018-12-30 22:18:18 +0800

    feign client

--- patient-service/src/main/java/com/sudharsan/serviceImpl/PatientProxyInterface.java
+++ patient-service/src/main/java/com/sudharsan/serviceImpl/PatientProxyInterface.java
@@ -1,18 +1,16 @@
 package com.sudharsan.serviceImpl;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.sudharsan.domain.Patient;
 
 @FeignClient(name="patient-proxy",url="http://localhost:8080/doctor")
 public interface PatientProxyInterface {
 	
-	@PostMapping(path="/addPatient",consumes=MediaType.APPLICATION_JSON_VALUE,
+	@PostMapping(path="/createDoctor",consumes=MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> addPatient(@RequestBody String patient);
 		
